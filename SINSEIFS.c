5158e47d4b72ef645594eb42baa1741ab872a045
azhar416 <rar.azhar.416@gmail.com>
2021-06-12 19:08:06 +0700

    capek

--- SINSEIFS.c
+++ SINSEIFS.c
@@ -9,6 +9,99 @@
 #include <sys/time.h>
  
 static const char *dirpath = "/home/azhar416/Downloads";
+static const char *sysLog = "/home/azhar416/SinSeiFS.log";
+
+void systemlog(char* level, char* cmd, int desctotal, const char* desc[])
+{
+    FILE* file = fopen(sysLog, "a");
+
+    time_t now;
+    time(&now);
+
+    struct tm* t = localtime(&now);
+    fprintf(file, "%s::%s::%02d%02d%04d-%02d:%02d:%02d", level, cmd, t->tm_mday, t->tm_mon+1, t->tm_year+1900, t->tm_hour, t->tm_min, t->tm_sec);
+    for (int i = 0; i < desctotal; i++)
+    {
+        fprintf(file, "::%s", desc[i]);
+    }
+    fprintf(file, "\n");
+    fclose(file);
+}
+
+static int xmp_rmdir(const char* path)
+{
+    char fpath[1000];
+    bzero(fpath, 1000);
+    int i, cekCipher = 0;
+ 
+    char* t;
+    if (strcmp(path, "/") != 0) {
+        t = strstr(path, "/AtoZ_");
+        if (t) {
+            cekCipher = 1;
+            t++;
+        }
+    }
+ 
+    if(strcmp(path, "/") == 0)
+    {
+        path = dirpath;
+        sprintf(fpath,"%s",path);
+    } 
+    else if (cekCipher) 
+    {
+        char befciper[1024];
+        bzero(befciper, 1024);
+        strncpy(befciper, path, strlen(path) - strlen(t));
+ 
+        char x[1024]; strcpy(x, t);
+        char* token;
+        char* s = x;
+        
+        char temp[1024];
+        i = 0;
+        while ((token = strtok_r(s, "/", &s))) 
+        {
+            bzero(temp, 1024);
+            if(i == 0) 
+            {
+                strcpy(temp, token);
+                strcat(befciper, temp);
+                
+                i = 1;
+                continue;
+            }
+
+            char str[1024];
+            strcpy(str, token);
+            int i;
+            for(i = 0; i < strlen(str); i++) 
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z') {
+                str[i] = 'Z' - (str[i] - 'A');
+                }
+                else if (str[i] >= 'a' && str[i] <= 'z') {
+                    str[i] = 'z' - (str[i] - 'a');
+                }
+            }
+            char* res = str;
+
+            strcpy(temp, res);
+            strcat(befciper, "/");
+            strcat(befciper, temp);
+        }
+        sprintf(fpath, "%s%s", dirpath, befciper); 
+    }
+    else sprintf(fpath, "%s%s",dirpath,path);
+
+    int res = rmdir(fpath);
+    if (res < 0)
+        return -errno;
+
+    const char *desc[] = {path};
+    systemlog("WARNING","RMDIR", 1, desc);
+    return 0;
+}
 
 /* XMP Field */
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
@@ -182,6 +275,9 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
     }
  
     closedir(dp);
+
+    const char* desc[] = {path};
+    systemlog("INFO", "READDIR", 1, desc);
     return 0;
 }
  
@@ -360,12 +456,15 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset, stru
         res = -errno;
  
     close(fd);
+
+    const char* desc[] = {path}; 
+    systemlog("INFO", "READ", 1, desc);
     return res;
 }
  
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
-    printf("PET : %s\n", path);
+    // printf("PET : %s\n", path);
     char fpath[1000];
     bzero(fpath, 1000);
     int i, cekCipher = 0;
@@ -528,6 +627,8 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
     if (res == -1)
         return -errno;
  
+    const char* desc[] = {path}; 
+    systemlog("INFO", "READ", 1, desc);
     return 0;
 }
 
@@ -702,6 +803,8 @@ static int xmp_mkdir(const char *path, mode_t mode)
     if (res < 0)
         return -errno;
     
+    const char* desc[] = {path}; 
+    systemlog("INFO", "MKDIR", 1, desc);
     return 0;
 }
 
@@ -1033,6 +1136,83 @@ static int xmp_rename(const char *from, const char *to)
     if (res < 0)
         return -errno;
 
+    const char* desc[] = {from, to}; 
+    systemlog("INFO", "RENAME", 2, desc);
+    return 0;
+}
+
+static int xmp_unlink(const char* path)
+{
+    char fpath[1000];
+    bzero(fpath, 1000);
+    int i, cekCipher = 0;
+ 
+    char* t;
+    if (strcmp(path, "/") != 0) {
+        t = strstr(path, "/AtoZ_");
+        if (t) {
+            cekCipher = 1;
+            t++;
+        }
+    }
+ 
+    if(strcmp(path, "/") == 0)
+    {
+        path = dirpath;
+        sprintf(fpath,"%s",path);
+    } 
+    else if (cekCipher) 
+    {
+        char befciper[1024];
+        bzero(befciper, 1024);
+        strncpy(befciper, path, strlen(path) - strlen(t));
+ 
+        char x[1024]; strcpy(x, t);
+        char* token;
+        char* s = x;
+        
+        char temp[1024];
+        i = 0;
+        while ((token = strtok_r(s, "/", &s))) 
+        {
+            bzero(temp, 1024);
+            if(i == 0) 
+            {
+                strcpy(temp, token);
+                strcat(befciper, temp);
+                
+                i = 1;
+                continue;
+            }
+
+            char str[1024];
+            strcpy(str, token);
+            int i;
+            for(i = 0; i < strlen(str); i++) 
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z') {
+                str[i] = 'Z' - (str[i] - 'A');
+                }
+                else if (str[i] >= 'a' && str[i] <= 'z') {
+                    str[i] = 'z' - (str[i] - 'a');
+                }
+            }
+            char* res = str;
+
+            strcpy(temp, res);
+            strcat(befciper, "/");
+            strcat(befciper, temp);
+        }
+        sprintf(fpath, "%s%s", dirpath, befciper); 
+    }
+    else sprintf(fpath, "%s%s",dirpath,path);
+
+    int res = unlink(fpath);
+    if (res < 0)
+        return -errno;
+    
+    const char *desc[] = {path};
+    systemlog("WARNING","UNLINK", 1, desc);
     return 0;
 }
  
@@ -1042,6 +1222,8 @@ static struct fuse_operations xmp_oper = {
     .read = xmp_read,
     .mkdir = xmp_mkdir,
     .rename = xmp_rename,
+    .rmdir = xmp_rmdir,
+    .unlink = xmp_unlink,
 };
 
 /* End XMP Field */
@@ -1053,3 +1235,5 @@ int  main(int  argc, char *argv[])
  
     return fuse_main(argc, argv, &xmp_oper, NULL);
 }
+
+// gcc -Wall `pkg-config fuse --cflags` SINSEIFS.c -o tes `pkg-config fuse --libs`
